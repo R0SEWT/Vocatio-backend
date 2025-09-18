70d48adf072737faf4e1ccd3126840036d0d9933
Rody Vilchez <102562850+R0SEWT@users.noreply.github.com>
2025-09-17 21:15:47 -0500

    init repo

---
+++ src/main/java/com/acme/vocatio/VocatioApplication.java
@@ -0,0 +1,13 @@
+package com.acme.vocatio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocatioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VocatioApplication.class, args);
+    }
+
+}
