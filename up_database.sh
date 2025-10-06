053decf21d51e15cd49168d0a126efc6424259cf
Rody Vilchez <102562850+R0SEWT@users.noreply.github.com>
2025-10-06 17:48:44 -0500

    fix error deploy

--- up_database.sh
+++ up_database.sh
@@ -1,14 +1,15 @@
 # sudo chmod +x up_database.sh
 # ./up_database.sh
 
-docker build -t vocatio .
-docker run --rm -p 8080:8080 \
-  -e SPRING_DATASOURCE_URL="jdbc:postgresql://host.docker.internal:5434/vocatio_db" \
+sudo docker build -t vocatio .
+sudo docker run --rm -p 8080:8080 \
+  --add-host=host.docker.internal:host-gateway \
+  -e SPRING_DATASOURCE_URL="jdbc:postgresql://host.docker.internal:5434/vocatio_db?sslmode=disable" \
   -e SPRING_DATASOURCE_USERNAME=postgres \
   -e SPRING_DATASOURCE_PASSWORD=password \
-  -e JWT_SECRET="cambia-esto" \
+  -e JWT_SECRET="dev-secret" \
+  -e PORT=8080 \
   vocatio
-
 
 # Trouble shooting
 
